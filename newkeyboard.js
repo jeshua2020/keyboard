b31eaafdd4a1b790fcd8be65e5e8284212765abf
Josh2020 <jeshuaadebayo@gmail.com>
2020-10-13 09:41:38 +0100

    keybord

---
+++ newkeyboard.js
@@ -0,0 +1,16 @@
+Const Keyboard ={
+    elements:{
+        main:null
+        keycontainer:null
+            keys:[]
+        }
+            
+        
+            
+    }
+    eventHandlers:{
+        oninput:null
+        onclick:null
+    };
+
+};
